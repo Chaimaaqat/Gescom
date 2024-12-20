6ba4cb9a6b004828165fc32492fc4833edcd6dad
Chaïmaa QATIF <cqati@lmdsio.com>
2024-12-20 15:32:46 +0100

    Initial commit

---
+++ src/metier/Categorie.java
@@ -0,0 +1,36 @@
+package metier;
+
+import java.util.*;
+
+public class Categorie {
+
+    private Integer idCategorie;
+    private String libCategorie;
+
+
+    public Integer getIdCategorie() {
+        return idCategorie;
+    }
+
+    public void setIdCategorie(Integer idCategorie) {
+        this.idCategorie = idCategorie;
+    }
+
+    public String getLibCategorie() {
+        return libCategorie;
+    }
+
+    public void setLibCategorie(String libCategorie) {
+        this.libCategorie = libCategorie;
+    }
+
+
+
+    public Categorie(int idCategorie, String libCategorie) {
+    setIdCategorie(idCategorie);
+    setLibCategorie(libCategorie);
+
+    }
+
+
+}
